e592bc1cbad017d61642d17903c3577150d92140
Rutger Schimmel <rma.schimmel@gmail.com>
2017-09-24 12:46:01 +0200

    Add default guitar strings

--- src/container/Fretboard.js
+++ src/container/Fretboard.js
@@ -7,17 +7,23 @@ import { String as StringModel, StringStore } from '../store/String';
 export default class Fretboard extends Component {
     componentWillMount() {
         this.stringStore = new StringStore();
+
+        const strings = [8, 3, 11, 6, 1, 8];
+        strings.forEach(this.addString);
     }
 
-    addString = () => {
-        const string = new StringModel();
+    addString = (tuningKey) => {
+        const string = new StringModel({
+            tuningKey: tuningKey || null,
+        });
+
         this.stringStore.add(string.toJS());
     }
 
     render() {
         return (
             <div>
-                <button onClick={this.addString} type="button">+</button>
+                <button onClick={() => this.addString()} type="button">+</button>
                 <div>
                     {this.stringStore.map(string =>
                         <String key={string.cid} string={string} />
